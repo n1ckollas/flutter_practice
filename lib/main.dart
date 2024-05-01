02ab7a66fa7d9ad75e639ace9402e2b3e6af3ea8
Mykola Rebezniuk <nick.rebez@gmail.com>
2024-04-30 21:49:21 -0400

    Seven is finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/07/screens/tabs.dart';
 
-import 'package:meals/00-OG/screens/tabs.dart';
 
 final theme = ThemeData(
-  useMaterial3: true,
-  colorScheme: ColorScheme.fromSeed(
+  colorScheme:ColorScheme.fromSeed(
     brightness: Brightness.light,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
-  textTheme: GoogleFonts.latoTextTheme(),
+  textTheme: GoogleFonts.latoTextTheme()
 );
 
-void main() {
+
+void main(){
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatelessWidget{
   const App({super.key});
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(context){
     return MaterialApp(
-      theme: theme,
+      theme:theme,
       home: const TabsScreen(),
     );
   }
