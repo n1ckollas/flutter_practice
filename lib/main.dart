4a9baa9f4efe32134abd32cbcbb78bff63081d1e
Mykola Rebezniuk <nick.rebez@gmail.com>
2024-05-02 18:46:53 -0400

    Eight is great

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/07/screens/tabs.dart';
-
+import 'package:meals/08/screens/tabs.dart';
 
 final theme = ThemeData(
-  colorScheme:ColorScheme.fromSeed(
+  colorScheme: ColorScheme.fromSeed(
     brightness: Brightness.light,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
-  textTheme: GoogleFonts.latoTextTheme()
+  textTheme: GoogleFonts.latoTextTheme(),
 );
-
 
 void main(){
   runApp(const App());
@@ -18,11 +16,12 @@ void main(){
 
 class App extends StatelessWidget{
   const App({super.key});
+
   @override
   Widget build(context){
     return MaterialApp(
-      theme:theme,
-      home: const TabsScreen(),
+      theme: theme,
+      home: const TabsScreen()
     );
   }
 }
